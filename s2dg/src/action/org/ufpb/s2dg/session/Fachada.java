f2bb649096f45d07d19952fe428b99e00d2101db
yuriggc <yuriggc@a5e8408c-40b6-11de-9cfd-b5a970547080>
2009-06-25 01:00:24 +0000

    menu discente dinamico os links das turmas agora alteram a aba Principal do discente

--- s2dg/src/action/org/ufpb/s2dg/session/Fachada.java
+++ s2dg/src/action/org/ufpb/s2dg/session/Fachada.java
@@ -1,10 +1,13 @@
 package org.ufpb.s2dg.session;
 
+import java.util.List;
+
 import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.AutoCreate;
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
+import org.ufpb.s2dg.entity.AlunoTurma;
 import org.ufpb.s2dg.entity.Usuario;
 
 @Name("fachada")
@@ -14,8 +17,11 @@ public class Fachada {
 	
 	@In
 	private UsuarioDAO usuarioDAO;
+	@In
+	private AlunoTurmaDAO alunoTurmaDAO;
 	
 	private Usuario usuario;
+	private AlunoTurma alunoTurmaAtual;
 
 	public Usuario getUsuario() {
 		return usuario;
@@ -27,6 +33,21 @@ public class Fachada {
 
 	public Usuario getUsuarioDoBanco(String username, String password) {
 		return usuarioDAO.getUsuario(username,password);
+	}
+	
+	public List<AlunoTurma> getAlunoTurmasDoBanco() {
+		List<AlunoTurma> alunoTurmas = alunoTurmaDAO.getAlunoTurmas(usuario);
+		if((alunoTurmas != null)&&(alunoTurmas.size() > 0))
+			alunoTurmaAtual = alunoTurmas.get(0);
+		return alunoTurmas;
+	}
+
+	public AlunoTurma getAlunoTurmaAtual() {
+		return alunoTurmaAtual;
+	}
+
+	public void setAlunoTurmaAtual(AlunoTurma alunoTurmaAtual) {
+		this.alunoTurmaAtual = alunoTurmaAtual;
 	}
 	
 }

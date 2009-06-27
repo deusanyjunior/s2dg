b49d7835c1ed8cb0ba671379e8b2819589025d50
dienertalencar <dienertalencar@a5e8408c-40b6-11de-9cfd-b5a970547080>
2009-06-27 20:22:07 +0000

    Separando Melhor os pacotes (criando pacote de persistência)

---
+++ trunk/s2dg/src/action/org/ufpb/s2dg/session/persistence/UsuarioDAO.java
@@ -0,0 +1,39 @@
+package org.ufpb.s2dg.session.persistence;
+
+import javax.persistence.EntityManager;
+
+import org.jboss.seam.annotations.AutoCreate;
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Name;
+import org.ufpb.s2dg.entity.Usuario;
+
+@AutoCreate
+@Name("usuarioDAO")
+public class UsuarioDAO {
+
+	@In
+	EntityManager entityManager;
+	
+	public Usuario getUsuario(String username, String password) {
+		return (Usuario) entityManager.createQuery(
+    	"select u from Usuario as u where u.cpf =:cpf and u.senha =:senha")
+    	.setParameter("cpf", username)
+    	.setParameter("senha", password)
+    	.getSingleResult();
+	}
+
+	public Usuario getUsuarioProfessor(String matricula) {
+		return (Usuario) entityManager.createQuery(
+    	"select u from Usuario as u where u.professor.matricula =:matricula")
+    	.setParameter("matricula", matricula)
+    	.getSingleResult();
+	}
+	
+	public Usuario getUsuarioAluno(String matricula) {
+		return (Usuario) entityManager.createQuery(
+    	"select u from Usuario as u where u.aluno.matricula =:matricula")
+    	.setParameter("matricula", matricula)
+    	.getSingleResult();
+	}
+	
+}

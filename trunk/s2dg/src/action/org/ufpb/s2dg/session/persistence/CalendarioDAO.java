149dc18681cd69640cc4232e05f5b51ac1d94001
dienertalencar <dienertalencar@a5e8408c-40b6-11de-9cfd-b5a970547080>
2009-07-06 03:00:27 +0000

    Tbm deve funcionar, caso esteja vazio

--- trunk/s2dg/src/action/org/ufpb/s2dg/session/persistence/CalendarioDAO.java
+++ trunk/s2dg/src/action/org/ufpb/s2dg/session/persistence/CalendarioDAO.java
@@ -1,5 +1,7 @@
 package org.ufpb.s2dg.session.persistence;
 
+import java.util.ArrayList;
+
 import javax.persistence.EntityManager;
 
 import org.jboss.seam.annotations.AutoCreate;
@@ -16,10 +18,13 @@ public class CalendarioDAO {
 	EntityManager entityManager;
 	
 	public Calendario getCalendario(Periodo periodo) {
-		return (Calendario) entityManager.createQuery(
-    	"select cal from Calendario as cal where cal.periodo = :periodo")
-    	.setParameter("periodo", periodo)
-    	.getSingleResult();
+		ArrayList<Calendario> result = 
+			(ArrayList<Calendario>) entityManager.createQuery(
+			"select cal from Calendario as cal where cal.periodo = :periodo")
+			.setParameter("periodo", periodo).getResultList();
+		if (result.size() > 0)
+			return result.get(0);
+		return null;
 	}
 
 }

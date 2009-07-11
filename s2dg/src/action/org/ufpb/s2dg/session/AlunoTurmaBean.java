d8ff5ea4794d5b301fa2403ca78e866fb0ed3dca
yuriggc <yuriggc@a5e8408c-40b6-11de-9cfd-b5a970547080>
2009-07-11 21:36:20 +0000

    Removidas as classes relacionadas com Nota, que não são mais necessárias. Criados os Beans para AlunoTurma, Calendario, DisciplinasMinistradas, Global, Turma, TurmasMatriculadas e Usuario. Ficam faltando beans para: 1) a lista de alunoTurma relacionada as médias/faltas dos alunos de uma turma; 2) lista de avaliações de uma turma; 3) lista de notas das avaliações dos alunos de uma turma (AlunoTurmaAvaliacao); 4) criação, edição e exclusão de avaliações de uma turma

---
+++ s2dg/src/action/org/ufpb/s2dg/session/AlunoTurmaBean.java
@@ -0,0 +1,24 @@
+package org.ufpb.s2dg.session;
+
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.AutoCreate;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+import org.ufpb.s2dg.entity.AlunoTurma;
+
+@Name("alunoTurmaBean")
+@Scope(ScopeType.PAGE)
+@AutoCreate
+public class AlunoTurmaBean {
+
+	private AlunoTurma alunoTurma;
+
+	public AlunoTurma getAlunoTurma() {
+		return alunoTurma;
+	}
+
+	public void setAlunoTurma(AlunoTurma alunoTurma) {
+		this.alunoTurma = alunoTurma;
+	}
+	
+}
---
+++ s2dg/src/action/org/ufpb/s2dg/session/TurmaBean.java
@@ -0,0 +1,24 @@
+package org.ufpb.s2dg.session;
+
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.AutoCreate;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+import org.ufpb.s2dg.entity.Turma;
+
+@Name("turmaBean")
+@Scope(ScopeType.PAGE)
+@AutoCreate
+public class TurmaBean {
+
+	Turma turma;
+
+	public Turma getTurma() {
+		return turma;
+	}
+
+	public void setTurma(Turma turma) {
+		this.turma = turma;
+	}
+	
+}

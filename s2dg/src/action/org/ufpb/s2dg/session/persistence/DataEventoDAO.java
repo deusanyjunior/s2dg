c48f44fbfe97c4b268578898b8365ed3eb4c180c
hildegardpaulino <hildegardpaulino@a5e8408c-40b6-11de-9cfd-b5a970547080>
2009-07-15 18:32:00 +0000

    Branch do componente calendário. Ainda não funciona, ms tá quase...

---
+++ s2dg/src/action/org/ufpb/s2dg/session/persistence/DataEventoDAO.java
@@ -0,0 +1,47 @@
+package org.ufpb.s2dg.session.persistence;
+
+import java.util.ArrayList;
+
+import javax.persistence.EntityManager;
+
+import org.jboss.seam.annotations.AutoCreate;
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Name;
+
+import org.ufpb.s2dg.entity.DataEvento;
+
+@AutoCreate
+@Name("dataeventoDAO")
+public class DataEventoDAO {
+	
+	@In
+	EntityManager entityManager;
+	
+	public ArrayList<DataEvento> getDataEvento(int mes, int ano) {
+		try {
+			if (entityManager == null) {
+				System.out.println("1111111111111111111");
+			}
+			else if (entityManager.createQuery(
+					"select de from DataEvento as de where EXTRACT(MONTH FROM de.data) = " + 4 + " and " +
+					"EXTRACT(YEAR FROM de.data) = " + 2009) == null) {
+				System.out.println("222222222222222222222222");
+			}
+			else  if((ArrayList<DataEvento>) entityManager.createQuery(
+					"select de from DataEvento as de where EXTRACT(MONTH FROM de.data) = " + 4 + " and " +
+					"EXTRACT(YEAR FROM de.data) = " + 2009).getResultList() == null) {
+				System.out.println("333333333333333333333333");
+			}
+			
+			return (ArrayList<DataEvento>) entityManager.createQuery(
+					"select de from DataEvento as de where EXTRACT(MONTH FROM de.data) = " + 4 + " and " +
+					"EXTRACT(YEAR FROM de.data) = " + 2009).getResultList();
+					//.setParameter("mes", mes).setParameter("ano", ano).getResultList();
+		}
+		catch(NullPointerException npe) {
+			npe.printStackTrace();
+			return new ArrayList<DataEvento>();
+		}
+		
+	}
+}

c067c47d7069bd5d1c67c3f7685d07e96b08320b
yuriggc <yuriggc@a5e8408c-40b6-11de-9cfd-b5a970547080>
2009-07-29 03:55:42 +0000

    Correção na ordenação de horários; Matrícula verifica creditos e choque de horário com disciplinas previamente matriculadas; Inclusão do número de vagas na visão; Renderização do botão selecionar disciplina na matrícula de acordo com a disponibilidade de vagas; Falta organizar a tabela pra agrupar turmas de uma mesma disciplina;

--- s2dg/src/action/org/ufpb/s2dg/session/HorarioComparator.java
+++ s2dg/src/action/org/ufpb/s2dg/session/HorarioComparator.java
@@ -7,21 +7,15 @@ import org.ufpb.s2dg.entity.Horario;
 public class HorarioComparator implements Comparator<Horario> {
 
 	public int compare(Horario h1, Horario h2) {
-		int result1 = h1.getDia().compareTo(h2.getDia());
-		if(result1 != 0)
-			return result1;
+		int result = h1.getDia().ordinal()-h2.getDia().ordinal();
+		if(result != 0)
+			return result;
 		else {
-			if(h1.getHoraInicio() < h2.getHoraInicio())
-				return -1;
-			else if(h1.getHoraInicio() > h2.getHoraInicio())
-				return 1;
+			result = h1.getHoraInicio()-h2.getHoraInicio();
+			if(result != 0)
+				return result;
 			else {
-				if(h1.getHoraFim() < h2.getHoraFim())
-					return -1;
-				else if(h1.getHoraFim() > h2.getHoraFim())
-					return 1;
-				else
-					return 0;
+				return h1.getMinutoInicio()-h2.getMinutoInicio();
 			}
 		}
 	}

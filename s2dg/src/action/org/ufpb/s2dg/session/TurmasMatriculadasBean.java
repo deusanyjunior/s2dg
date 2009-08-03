c296f5ca8348f63e352695bf4654252bedf44d70
danctor <danctor@a5e8408c-40b6-11de-9cfd-b5a970547080>
2009-08-03 00:56:26 +0000

    Alteracoes para geracao de PDF do historico

--- s2dg/src/action/org/ufpb/s2dg/session/TurmasMatriculadasBean.java
+++ s2dg/src/action/org/ufpb/s2dg/session/TurmasMatriculadasBean.java
@@ -17,8 +17,11 @@ import org.ufpb.s2dg.entity.Disciplina;
 import org.ufpb.s2dg.entity.Horario;
 import org.ufpb.s2dg.entity.Sala;
 import org.ufpb.s2dg.entity.Turma;
+import org.ufpb.s2dg.entity.Aluno.FormaIngresso;
+import org.ufpb.s2dg.entity.Aluno.SituacaoAcademica;
 import org.ufpb.s2dg.entity.AlunoTurma.Situacao;
 import org.ufpb.s2dg.entity.Disciplina.Tipo;
+import org.ufpb.s2dg.session.persistence.AlunoDAO;
 
 @Name("turmasMatriculadasBean")
 @Scope(ScopeType.SESSION)
@@ -38,6 +41,12 @@ public class TurmasMatriculadasBean {
 	
 	@In
 	PDFAction pdfAction;
+	
+	@In
+	AlunoDAO alunoDAO;
+	
+	@In
+	UsuarioBean usuarioBean;
 		
 	public void init() {
 		List<AlunoTurma> ats = fachada.getAlunoTurmaDoBanco();
@@ -320,23 +329,161 @@ public class TurmasMatriculadasBean {
 	
 	public void exportarHistoricoPDF() {
 		ArrayList<HashMap<String, String>> mapas = new ArrayList<HashMap<String, String>>();
+		
+		// Matricula - Nome do Aluno - Codigo do Curso - Nome do Curso - Numero do Curriculo - Reconhecimento - RG
+		HashMap<String, String> mapaCabecalho = new HashMap<String, String>();
+		mapaCabecalho.put("Matricula", fachada.getAluno().getMatricula());
+		mapaCabecalho.put("Nome", fachada.getUsuario().getNome());
+		mapaCabecalho.put("Curso", fachada.getAluno().getCurriculo().getCurso().getCodigo());
+		mapaCabecalho.put("Nome", fachada.getAluno().getCurriculo().getCurso().getNome());
+		mapaCabecalho.put("Curriculo", fachada.getAluno().getCurriculo().getNumero());
+		mapaCabecalho.put("Reconhecimento", fachada.getAluno().getCurriculo().getCurso().getCur_ato_criacao());
+		mapaCabecalho.put("RG", fachada.getUsuario().getRg());
+		mapas.add(mapaCabecalho);
+		
 		//CODIGO - NOME DA DISCIPLINA - CR - CH.- PERIODO - MEDIA -SITUACAO 
+		//Disciplinas Obrigatorias
 		for (AlunoTurma at : alunoTurmas) {
-			HashMap<String, String> mapa = new HashMap<String, String>();
-			mapa.put("Codigo", at.getTurma().getNumero());
-			mapa.put("Disciplina", at.getTurma().getDisciplina().getCodigo());
-			int creditos = at.getTurma().getDisciplina().getCreditos();
-			mapa.put("Creditos", String.valueOf(creditos));
-			mapa.put("Carga Horaria", String.valueOf(creditos*15));			
-			mapa.put("Periodo", at.getTurma().getPeriodo().getAno().concat(" "+ at.getTurma().getPeriodo().getSemestre()));
-			mapa.put("Media",turmasMatriculadasBean.geraMedia(at)+"");
-			mapa.put("Situacao", at.getSituacao()+"");
-			
+			if(at.getTurma().getDisciplina().getTipo()==Tipo.OBRIGATORIA){
+				HashMap<String, String> mapaDisciplinasObrigatorias = new HashMap<String, String>();
+				mapaDisciplinasObrigatorias.put("Codigo", at.getTurma().getNumero());
+				mapaDisciplinasObrigatorias.put("Disciplina", at.getTurma().getDisciplina().getCodigo());
+				int creditos = at.getTurma().getDisciplina().getCreditos();
+				mapaDisciplinasObrigatorias.put("Creditos", String.valueOf(creditos));
+				mapaDisciplinasObrigatorias.put("Carga Horaria", String.valueOf(creditos*15));			
+				mapaDisciplinasObrigatorias.put("Periodo", at.getTurma().getPeriodo().getAno().concat(" "+ at.getTurma().getPeriodo().getSemestre()));
+				mapaDisciplinasObrigatorias.put("Media",turmasMatriculadasBean.geraMedia(at)+"");
+				mapaDisciplinasObrigatorias.put("Situacao", at.getSituacao()+"");
 						
-			mapas.add(mapa);
+				mapas.add(mapaDisciplinasObrigatorias);
+			}
 		}
 		
-		pdfAction.geraPdf("Historico Escolar", mapas);
+		//Disciplinas Optativas
+		for (AlunoTurma at : alunoTurmas) {
+			if(at.getTurma().getDisciplina().getTipo()==Tipo.OPTATIVA){
+				HashMap<String, String> mapaDisciplinasOptativas = new HashMap<String, String>();
+				mapaDisciplinasOptativas.put("Codigo", at.getTurma().getNumero());
+				mapaDisciplinasOptativas.put("Disciplina", at.getTurma().getDisciplina().getCodigo());
+				int creditos = at.getTurma().getDisciplina().getCreditos();
+				mapaDisciplinasOptativas.put("Creditos", String.valueOf(creditos));
+				mapaDisciplinasOptativas.put("Carga Horaria", String.valueOf(creditos*15));			
+				mapaDisciplinasOptativas.put("Periodo", at.getTurma().getPeriodo().getAno().concat(" "+ at.getTurma().getPeriodo().getSemestre()));
+				mapaDisciplinasOptativas.put("Media",turmasMatriculadasBean.geraMedia(at)+"");
+				mapaDisciplinasOptativas.put("Situacao", at.getSituacao()+"");
+						
+				mapas.add(mapaDisciplinasOptativas);
+			}
+		}
+		
+		//Disciplinas Complementares
+		for (AlunoTurma at : alunoTurmas) {
+			if(at.getTurma().getDisciplina().getTipo()==Tipo.COMPLEMENTAR){
+				HashMap<String, String> mapaDisciplinasComplementares = new HashMap<String, String>();
+				mapaDisciplinasComplementares.put("Codigo", at.getTurma().getNumero());
+				mapaDisciplinasComplementares.put("Disciplina", at.getTurma().getDisciplina().getCodigo());
+				int creditos = at.getTurma().getDisciplina().getCreditos();
+				mapaDisciplinasComplementares.put("Creditos", String.valueOf(creditos));
+				mapaDisciplinasComplementares.put("Carga Horaria", String.valueOf(creditos*15));			
+				mapaDisciplinasComplementares.put("Periodo", at.getTurma().getPeriodo().getAno().concat(" "+ at.getTurma().getPeriodo().getSemestre()));
+				mapaDisciplinasComplementares.put("Media",turmasMatriculadasBean.geraMedia(at)+"");
+				mapaDisciplinasComplementares.put("Situacao", at.getSituacao()+"");
+						
+				mapas.add(mapaDisciplinasComplementares);
+			}
+		}
+		//Integralizacoes Disciplina Obrigatoria
+		HashMap<String, String> mapaIntegralizacaoObrigatoria = new HashMap<String, String>();
+		mapaIntegralizacaoObrigatoria.put("Carga Horaria Minima", cargaHoraria(fachada.getAluno().getCurriculo().getMinimoCreditosCurriculo())+"");
+		mapaIntegralizacaoObrigatoria.put("Integralizada", cargaHoraria(geraCreditosIntegralizados())+"");
+		mapaIntegralizacaoObrigatoria.put("Creditos Minimo", fachada.getAluno().getCurriculo().getMinimoCreditosCurriculo()+"");
+		mapaIntegralizacaoObrigatoria.put("Integralizado", fachada.getAluno().getCurriculo().getMinimoCreditosCurriculo()+"");
+		mapaIntegralizacaoObrigatoria.put("Disciplinas Minimo", fachada.getAluno().getCurriculo().getMinimoDisciplinas()+"");
+		mapaIntegralizacaoObrigatoria.put("Integralizado", alunoTurmas.size()+"");
+		mapas.add(mapaIntegralizacaoObrigatoria);
+		
+		//Integralizacoes Disciplina Optativa
+		HashMap<String, String> mapaIntegralizacaoOptativa = new HashMap<String, String>();
+		mapaIntegralizacaoOptativa.put("Carga Horaria Minima", cargaHoraria(fachada.getAluno().getCurriculo().getMinimoCreditosCurriculo())+"");
+		mapaIntegralizacaoOptativa.put("Integralizada", cargaHoraria(geraCreditosIntegralizados())+"");
+		mapaIntegralizacaoOptativa.put("Creditos Minimo", fachada.getAluno().getCurriculo().getMinimoCreditosCurriculo()+"");
+		mapaIntegralizacaoOptativa.put("Integralizado", fachada.getAluno().getCurriculo().getMinimoCreditosCurriculo()+"");
+		mapaIntegralizacaoOptativa.put("Disciplinas Minimo", fachada.getAluno().getCurriculo().getMinimoDisciplinas()+"");
+		mapaIntegralizacaoOptativa.put("Integralizado", alunoTurmas.size()+"");
+		mapas.add(mapaIntegralizacaoOptativa);
+		
+		//Integralizacoes Disciplina Complementar
+		HashMap<String, String> mapaIntegralizacaoComplementar = new HashMap<String, String>();
+		mapaIntegralizacaoComplementar.put("Carga Horaria Minima", cargaHoraria(fachada.getAluno().getCurriculo().getMinimoCreditosCurriculo())+"");
+		mapaIntegralizacaoComplementar.put("Integralizada", cargaHoraria(geraCreditosIntegralizados())+"");
+		mapaIntegralizacaoComplementar.put("Creditos Minimo", fachada.getAluno().getCurriculo().getMinimoCreditosCurriculo()+"");
+		mapaIntegralizacaoComplementar.put("Integralizado", fachada.getAluno().getCurriculo().getMinimoCreditosCurriculo()+"");
+		mapaIntegralizacaoComplementar.put("Disciplinas Minimo", fachada.getAluno().getCurriculo().getMinimoDisciplinas()+"");
+		mapaIntegralizacaoComplementar.put("Integralizado", alunoTurmas.size()+"");
+		mapas.add(mapaIntegralizacaoComplementar);
+		
+		//Integralizacao Total
+		HashMap<String, String> mapaIntegralizacaoTotal = new HashMap<String, String>();
+		mapaIntegralizacaoTotal.put("Carga Horaria Minima", cargaHoraria(fachada.getAluno().getCurriculo().getMinimoCreditosCurriculo())+"");
+		mapaIntegralizacaoTotal.put("Integralizada", cargaHoraria(geraCreditosIntegralizados())+"");
+		mapaIntegralizacaoTotal.put("Creditos Minimo", fachada.getAluno().getCurriculo().getMinimoCreditosCurriculo()+"");
+		mapaIntegralizacaoTotal.put("Integralizado", fachada.getAluno().getCurriculo().getMinimoCreditosCurriculo()+"");
+		mapaIntegralizacaoTotal.put("Disciplinas Minimo", fachada.getAluno().getCurriculo().getMinimoDisciplinas()+"");
+		mapaIntegralizacaoTotal.put("Integralizado", alunoTurmas.size()+"");
+		mapas.add(mapaIntegralizacaoTotal);
+
+		//Semestres cursados
+		HashMap<String, String> mapaSemestresCursados = new HashMap<String, String>();
+		mapaSemestresCursados.put("Cursados", geraSemestresCursados(getDisciplinasOrdenadas(alunoDAO.getAlunos(usuarioBean.getUsuario().getAluno().getMatricula())))+"");
+		mapaSemestresCursados.put("Minimo", fachada.getAluno().getCurriculo().getMinimoSemestres()+"");
+		mapaSemestresCursados.put("Maximo", fachada.getAluno().getCurriculo().getMaximoSemestres()+"");
+		mapaSemestresCursados.put("Ativos", geraSemestresCursados(getDisciplinasOrdenadas(alunoDAO.getAlunos(usuarioBean.getUsuario().getAluno().getMatricula())))+"");
+		mapas.add(mapaSemestresCursados);
+		
+		//Trancamentos Totais
+		HashMap<String, String> mapaTrancamentosTotais = new HashMap<String, String>();
+		mapaTrancamentosTotais.put("Trancamentos", geraTrancamentosTotais(getDisciplinasOrdenadas(alunoDAO.getAlunos(usuarioBean.getUsuario().getAluno().getMatricula())))+"");
+		mapaTrancamentosTotais.put("Maximo", fachada.getAluno().getCurriculo().getTrancamentosTotais()+"");
+		mapas.add(mapaTrancamentosTotais);
+		
+		//Matriculas Institucionais
+		HashMap<String, String> mapaMatriculasInstitucionais = new HashMap<String, String>();
+		mapaMatriculasInstitucionais.put("Matriculas", geraTrancamentosTotais(getDisciplinasOrdenadas(alunoDAO.getAlunos(usuarioBean.getUsuario().getAluno().getMatricula())))+"");
+		mapaMatriculasInstitucionais.put("Maximo", fachada.getAluno().getCurriculo().getMatriculaInstitucional()+"");
+		mapas.add(mapaMatriculasInstitucionais);
+
+		//Trancamentos Parciais
+		HashMap<String, String> mapaTrancamentosParciais = new HashMap<String, String>();
+		mapaTrancamentosParciais.put("Trancamentos", geraTrancamentosParciais()+"");
+		mapaTrancamentosParciais.put("Maximo", fachada.getAluno().getCurriculo().getMinimoCreditos()+"");
+		mapas.add(mapaTrancamentosParciais);
+		
+		//Creditos Matriculados
+		HashMap<String, String> mapaCreditosMatriculados = new HashMap<String, String>();
+		mapaCreditosMatriculados.put("Matriculados", geraCreditosIntegralizados()+"");
+		mapaCreditosMatriculados.put("Minimo", fachada.getAluno().getCurriculo().getMinimoCreditos()+"");
+		mapaCreditosMatriculados.put("Maximo", fachada.getAluno().getCurriculo().getMaximoCreditos()+"");
+		mapas.add(mapaCreditosMatriculados);
+		
+		//Situacao Academica
+		HashMap<String, String> mapaSituacaoAcademica = new HashMap<String, String>();
+		mapaSituacaoAcademica.put("Situacao", fachada.getAluno().getSituacaoAcademica()+"");
+		mapaSituacaoAcademica.put("CRE", geraCRE()+"");
+		if(fachada.getAluno().getSituacaoAcademica()==SituacaoAcademica.GRADUADO){
+			mapaSituacaoAcademica.put("Ano de Conclusao", getDisciplinasOrdenadas(alunoDAO.getAlunos(usuarioBean.getUsuario().getAluno().getMatricula())).get(0).getTurma().getPeriodo().getAno().
+					  concat(" "+getDisciplinasOrdenadas(alunoDAO.getAlunos(usuarioBean.getUsuario().getAluno().getMatricula())).get(0).getTurma().getPeriodo().getSemestre()));
+		}
+		mapaSituacaoAcademica.put("Forma de Ingresso", fachada.getAluno().getFormaIngresso()+"");
+		mapaSituacaoAcademica.put("Ano Ingresso", getDisciplinasOrdenadas(alunoDAO.getAlunos(usuarioBean.getUsuario().getAluno().getMatricula())).get(0).getTurma().getPeriodo().getAno().
+								  concat(" "+getDisciplinasOrdenadas(alunoDAO.getAlunos(usuarioBean.getUsuario().getAluno().getMatricula())).get(0).getTurma().getPeriodo().getSemestre()));
+		mapas.add(mapaSituacaoAcademica);
+		
+		//Notas do Vestibular
+		if(fachada.getAluno().getFormaIngresso()==FormaIngresso.VESTIBULAR){
+			
+		}
+		
+		//pdfAction.geraPdfHistorico("Historico Escolar", mapas);
 	}
 
 }

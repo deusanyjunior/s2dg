6adfe38e9cc3438e4d94fa22a55c8c8d3dd6c14c
rennandf <rennandf@a5e8408c-40b6-11de-9cfd-b5a970547080>
2009-08-13 18:19:31 +0000

    Co-Requesitos e pré requesitos implementados

--- trunk/s2dg/src/action/org/ufpb/s2dg/session/persistence/DisciplinaDAO.java
+++ trunk/s2dg/src/action/org/ufpb/s2dg/session/persistence/DisciplinaDAO.java
@@ -1,6 +1,8 @@
 package org.ufpb.s2dg.session.persistence;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
@@ -39,10 +41,27 @@ public class DisciplinaDAO {
 		return null;
 	}
 
-	public List<Disciplina> getCoRequisitos(Disciplina d) {
-		Query q = entityManager.createQuery("select d from Disciplina as d where :disciplina member of d.co_requisitos")
+	@SuppressWarnings("unchecked")
+	public Set<Disciplina> getPreRequisitos(Curriculo c, Disciplina d) {
+		Query q = entityManager.createQuery("select pr.dependencia from PreRequesito as pr where pr.curriculo = :curriculo and pr.disciplina = :disciplina")
+		.setParameter("curriculo", c)
+		.setParameter("disciplina", d);
+		Set<Disciplina> set = new HashSet<Disciplina>();
+		set.addAll(q.getResultList());
+		if(set.size() > 0)
+			return set;
+		return null;
+	}
+	
+	public List<Disciplina> getCoRequisitos(Curriculo c, Disciplina d) {
+		Query q = entityManager.createQuery("select co.disciplina2 from CoRequesito as co where co.curriculo = :curriculo and pr.disciplina1 = :disciplina")
+		.setParameter("curriculo", c)
 		.setParameter("disciplina", d);
 		List<Disciplina> list = q.getResultList();
+		q = entityManager.createQuery("select co.disciplina1 from CoRequesito as co where co.curriculo = :curriculo and pr.disciplina2 = :disciplina")
+		.setParameter("curriculo", c)
+		.setParameter("disciplina", d);		
+		list.addAll(q.getResultList());
 		if(list.size() > 0)
 			return list;
 		return null;

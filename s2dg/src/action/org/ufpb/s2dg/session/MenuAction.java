7e1ec841a93df30a34371c77843d8474e41af5ec
yuriggc <yuriggc@a5e8408c-40b6-11de-9cfd-b5a970547080>
2009-08-19 05:50:45 +0000

    Correção parcial das mensagens da alteração de senha e pergunta/resposta.

--- s2dg/src/action/org/ufpb/s2dg/session/MenuAction.java
+++ s2dg/src/action/org/ufpb/s2dg/session/MenuAction.java
@@ -22,7 +22,7 @@ public class MenuAction {
 	public int Id_Menu2 = 0;
 
 	String[] lista_opcoes2 = {"/corpo_do_docente.xhtml", 
-							 	"/configuracoes.xhtml"};
+								"/configuracoes.xhtml"};
 	
 	//Menu Discente
 	public String getOption() {

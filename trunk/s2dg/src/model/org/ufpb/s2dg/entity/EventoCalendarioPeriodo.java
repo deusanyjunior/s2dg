43b839eac3dd45fc3241ced394ca65b961582525
elenilson@elenilsonvieira.com <elenilson@elenilsonvieira.com@a5e8408c-40b6-11de-9cfd-b5a970547080>
2009-11-14 16:23:38 +0000

    Atualizado para buscar na nova tabela. É necessário ter o novo sql com os dados ou copiar todos os dados de dataevento para a tabela eventocalendarioperiodo.

--- trunk/s2dg/src/model/org/ufpb/s2dg/entity/EventoCalendarioPeriodo.java
+++ trunk/s2dg/src/model/org/ufpb/s2dg/entity/EventoCalendarioPeriodo.java
@@ -71,5 +71,40 @@ public class EventoCalendarioPeriodo /*extends EventoCalendario */implements Ser
 	public void setEvento(String evento) {
 		this.evento = evento;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((data == null) ? 0 : data.hashCode());
+		result = prime * result + ((evento == null) ? 0 : evento.hashCode());
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		EventoCalendarioPeriodo other = (EventoCalendarioPeriodo) obj;
+		if (data == null) {
+			if (other.data != null)
+				return false;
+		} else if (!data.equals(other.data))
+			return false;
+		if (evento == null) {
+			if (other.evento != null)
+				return false;
+		} else if (!evento.equals(other.evento))
+			return false;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	
 	
 }

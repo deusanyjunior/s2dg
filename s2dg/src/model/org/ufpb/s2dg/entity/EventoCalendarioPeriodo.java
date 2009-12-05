a2c4ae98d96ef18d0c8fecc712cb69fc73c7f6f0
liwher <liwher@a5e8408c-40b6-11de-9cfd-b5a970547080>
2009-12-05 22:32:14 +0000

    EventoCalendarioPeriodo herdando de EventoCalendario

--- s2dg/src/model/org/ufpb/s2dg/entity/EventoCalendarioPeriodo.java
+++ s2dg/src/model/org/ufpb/s2dg/entity/EventoCalendarioPeriodo.java
@@ -3,108 +3,25 @@ package org.ufpb.s2dg.entity;
 import java.io.Serializable;
 import java.util.Date;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 
-import org.hibernate.validator.Length;
-import org.hibernate.validator.NotNull;
 
-/**
- * EventoCalendarioPeriodo.java DEVERIA herdar de EventoCalendario
- * 
- * Representa um evento de calendário do periodo. Foi mal ae, eu tentei usar heranca, mas o Seam nao deixou! 
- */
 @Entity
 @Table(name = "eventocalendarioperiodo")
-public class EventoCalendarioPeriodo /*extends EventoCalendario */implements Serializable{
+public class EventoCalendarioPeriodo extends EventoCalendario implements Serializable{
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 5454532553653097257L;
-	private long id;
-	private Date data;
-	private String evento;
 	
 	public EventoCalendarioPeriodo() {
 		// TODO Auto-generated constructor stub
 	}
 	
-	public EventoCalendarioPeriodo(Date date, String event) {
-		setData(data);
-		setEvento(evento);
-	}
-	
-	@Id
-	@GeneratedValue(strategy=GenerationType.SEQUENCE)
-	@Column(name = "id", unique = true, nullable = false)
-	public long getId() {
-		return id;
-	}
-	
-	public void setId(long id) {
-		this.id = id;
-	}
-
-	@NotNull
-	@Temporal(TemporalType.DATE)
-	public Date getData() {
-		return data;
-	}
-	
-	public void setData(Date data) {
-		this.data = data;
-	}
-	
-	@NotNull
-	@Length(min=5, message="A descrição do evento deve ter no mínimo 5 caracteres")
-	public String getEvento() {
-		return evento;
-	}
-	
-	public void setEvento(String evento) {
-		this.evento = evento;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((data == null) ? 0 : data.hashCode());
-		result = prime * result + ((evento == null) ? 0 : evento.hashCode());
-		result = prime * result + (int) (id ^ (id >>> 32));
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		EventoCalendarioPeriodo other = (EventoCalendarioPeriodo) obj;
-		if (data == null) {
-			if (other.data != null)
-				return false;
-		} else if (!data.equals(other.data))
-			return false;
-		if (evento == null) {
-			if (other.evento != null)
-				return false;
-		} else if (!evento.equals(other.evento))
-			return false;
-		if (id != other.id)
-			return false;
-		return true;
-	}
-	
+	public EventoCalendarioPeriodo(Date date) {
+		super(date, EventoCalendario.TipoData.EVENTO_PERIODO.toString());
+	}	
 	
 }
